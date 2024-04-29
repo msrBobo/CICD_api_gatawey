d07924486a1d71d98b492f903658317749fe477f
Mubina <mubinayigitaliyeva00@gmail.com>
2024-04-29 09:57:37 +0500

    swagger updat

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -510,8 +510,6 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
-	LeftDelim:        "{{",
-	RightDelim:       "}}",
 }
 
 func init() {
