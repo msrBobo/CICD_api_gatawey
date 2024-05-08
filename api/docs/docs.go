278687152b8a049cb048cf771f0a8d5838be0af0
Mubina <mubinayigitaliyeva00@gmail.com>
2024-05-08 18:40:50 +0500

    doctor note api

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -1449,8 +1449,6 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
-	LeftDelim:        "{{",
-	RightDelim:       "}}",
 }
 
 func init() {
